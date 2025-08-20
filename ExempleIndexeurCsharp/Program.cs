f197de47b93eeadd199872904c3642063ad80eaa
michael person <michael.person@cognitic.be>
2025-08-20 09:34:00 +0200

    Add project files.

---
+++ ExempleIndexeurCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExempleIndexeurCsharp;
+
+Console.WriteLine("Exemple d'indexeur - Recette");
+Recette cakeAuxPommes = new Recette();
+cakeAuxPommes.Nom = "Cake aux Pommes façon grand-mère";
+cakeAuxPommes.Description = "Retrouvez le goût de votre enfance avec cette recette ancestrale du cake aux pommes traditionnel";
+cakeAuxPommes[0] = "Beurre";
+cakeAuxPommes[0] = "Farine";
+cakeAuxPommes[0] = "Eau pétillante";
+cakeAuxPommes[0] = "Des pommes congelées de la marque Malbouffe";
+cakeAuxPommes[0] = "sucre de canne";
+cakeAuxPommes[0] = "sucre blanc";
+cakeAuxPommes[0] = "sucre perlé";
+cakeAuxPommes[0] = "sucre vanillé";
+
+Console.WriteLine("Affichage de la recette");
+Console.WriteLine(cakeAuxPommes.Nom);
+Console.WriteLine(cakeAuxPommes.Description);
+Console.WriteLine("Ingrédients");
+ 
+for(int i = 0;i< cakeAuxPommes.NbIngredients;i++)
+{
+    Console.WriteLine($"{i}: {cakeAuxPommes[i]}");
+}
---
+++ ExempleIndexeurCsharp/Recette.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleIndexeurCsharp
+{
+    public class Recette
+    {
+        string _nom;
+        string _description;
+        List<string> _ingredients;
+
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+
+            set
+            {
+                _nom = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+
+            set
+            {
+                _description = value;
+            }
+        }
+
+        public int NbIngredients
+        {
+            get { return _ingredients.Count; }
+        }
+
+        public string this[int index]
+        {
+            get 
+            {
+               string? retour = null;
+               if(_ingredients == null) { return retour; }
+               return _ingredients[index];
+            }
+            set {
+                _ingredients = _ingredients ?? new List<string>();
+                if (_ingredients.Contains(value)) {
+                    Console.WriteLine("Imbecile tu as déjà donné cet ingrédient va dormir!!!");
+                }
+                _ingredients.Add("");
+                _ingredients[index]=(value);
+            
+            }
+        }
+    }
+}
